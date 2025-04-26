cbb607138dd11cfd0793aac24707b066c3972a27
Felipe Alves <felipealves0891@gmail.com>
2025-04-26 17:40:33 -0300

    Adicionado interações com usuario

--- src/Reax/Debugger/ReaxDebugger.cs
+++ src/Reax/Debugger/ReaxDebugger.cs
@@ -8,11 +8,13 @@ public class ReaxDebugger
 {
     public static Table<DebuggerModel> Table { get; private set; } = new();
     public static bool ToNextLine { get; private set; } = false;
-    
+    public static ConsoleKey[] Actions = [ConsoleKey.DownArrow, ConsoleKey.Enter, ConsoleKey.LeftArrow];
+
     static ReaxDebugger() 
     {
-        Table.AddItemMenu("Seta para baixo", "Linha a linha");
-        Table.AddItemMenu("Enter", "Continuar");
+        Table.AddItemMenu(ConsoleKey.DownArrow.ToString(), "Linha a linha");
+        Table.AddItemMenu(ConsoleKey.RightArrow.ToString(), "Continuar");
+        Table.AddItemMenu(ConsoleKey.Enter.ToString(), "Continuar até o fim!");
     }
 
     public static void Debugger(DebuggerArgs args)
@@ -33,10 +35,34 @@ public class ReaxDebugger
         Console.WriteLine();
         Console.WriteLine(source);
         PrintStackTrace(args.StackTrace);
+        Execute();
+    }
 
+    private static void Execute() 
+    {
         ToNextLine = false;
-        if(Console.ReadKey().Key == ConsoleKey.DownArrow)
-            ToNextLine = true;
+        
+        ConsoleKey key;
+        do
+        {
+            key = Console.ReadKey().Key;
+            if(key == ConsoleKey.DownArrow)
+            {
+                ToNextLine = true;
+                break;
+            }
+            else if(key == ConsoleKey.RightArrow)
+            {
+                break;
+            }
+            else if(key == ConsoleKey.Enter)
+            {
+                ReaxEnvironment.BreakPoints.Clear();
+                break;
+            }
+        }
+        while(!Actions.Contains(key));
+
     }
 
     public static void PrintStackTrace(ConcurrentStack<ReaxNode> StackTrace) {
