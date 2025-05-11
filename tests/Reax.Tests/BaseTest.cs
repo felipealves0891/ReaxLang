9320f71c142a98e5e16dd1c91457f565b905b337
Felipe Alves <felipealves0891@gmail.com>
2025-05-10 22:59:57 -0300

    Adicionado testes

--- tests/Reax.Tests/BaseTest.cs
+++ tests/Reax.Tests/BaseTest.cs
@@ -1,8 +1,38 @@
 using System;
+using Moq;
+using Reax.Lexer;
+using Reax.Parser.NodeParser;
 
 namespace Reax.Tests;
 
 public abstract class BaseTest<Tested>
 {
     protected abstract Tested CreateTested();
+
+    public ITokenSource CreateTokenSource(Token[] tokens, Action<IMock<ITokenSource>, int>? configuration = null) 
+    {
+        var mockedTokenSource = new Mock<ITokenSource>();
+        var position = 0;
+        var defaultToken = new Token(TokenType.UNKNOW, (byte)' ', "", 0, 0);
+
+        mockedTokenSource.Setup(x => x.Advance())
+                         .Callback(() => {
+                            position++;
+                         });
+
+        mockedTokenSource.Setup(x => x.BeforeToken)
+                         .Returns(() => position < tokens.Length ? tokens[position] : defaultToken);
+
+        mockedTokenSource.Setup(x => x.CurrentToken)
+                         .Returns(() => tokens[position]);
+                         
+        mockedTokenSource.Setup(x => x.NextToken)
+                         .Returns(() => position+1 > tokens.Length ? tokens[position] : defaultToken);
+
+        mockedTokenSource.Setup(x => x.EndOfTokens)
+                         .Returns(() => position >= tokens.Length);
+
+        configuration?.Invoke(mockedTokenSource, position);
+        return mockedTokenSource.Object;
+    }
 }
---
+++ tests/Reax.Tests/Parser/Helper/ParameterHelperTest.cs
@@ -0,0 +1,37 @@
+using System;
+using Moq;
+using Reax.Lexer;
+using Reax.Parser.Helper;
+using Reax.Parser.NodeParser;
+
+namespace Reax.Tests.Parser.Helper;
+
+public class ParameterHelperTest : BaseTest<ParameterHelper>
+{
+    [Fact]
+    public void GetParameters_HappyWay() 
+    {
+        //Arrange
+        var _mockedTokenSource = CreateTokenSource([
+            new Token(TokenType.START_PARAMETER, (byte)' ', "", 0, 0),
+            new Token(TokenType.IDENTIFIER, (byte)'m', "", 0, 0),
+            new Token(TokenType.IDENTIFIER, (byte)'m', "", 0, 0),
+            new Token(TokenType.INT_TYPE, (byte)' ', "", 0, 0),
+            new Token(TokenType.END_PARAMETER, (byte)' ', "", 0, 0),
+        ]);
+
+        //Act
+        var parameters = ParameterHelper.GetParameters(_mockedTokenSource);
+
+        //Assert
+        Assert.NotEmpty(parameters);
+        Assert.Single(parameters);
+        var single = parameters.Single();
+        Assert.Equal("m", single.Identifier);
+    }
+
+    protected override ParameterHelper CreateTested()
+    {
+        throw new NotImplementedException();
+    }
+}
