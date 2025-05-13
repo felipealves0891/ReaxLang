3fade8d41dce3c446d11a4179c4a81690ae954a0
Felipe Alves <felipealves0891@gmail.com>
2025-05-12 23:23:10 -0300

    Limpando o projeto

--- src/Reax.Core/ReaxEnvironment.cs
+++ src/Reax.Core/ReaxEnvironment.cs
@@ -5,9 +5,6 @@ namespace Reax.Core;
 
 public static class ReaxEnvironment
 {
-    public static bool Debug { get; set; } = false;
-    public static IReaxInterpreter? MainInterpreter { get; set; }
     public static string DirectoryRoot { get; set; } = string.Empty;
     public static Dictionary<string, ScriptNode> ImportedFiles { get; set; } = new();
-    public static IDictionary<string, ISet<int>> BreakPoints = new Dictionary<string, ISet<int>>();
 }
--- src/Reax/Program.cs
+++ src/Reax/Program.cs
@@ -1,7 +1,1 @@
-﻿using Reax.Commands;
-using Spectre.Console.Cli;
-
-var app = new CommandApp<RunCommand>();
-return app.Run(args);
-
-// dotnet run -- "D:\Source\Scripts\simple.reax" -b
+﻿Console.WriteLine("Hello world!");
