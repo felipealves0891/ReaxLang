269fce7b83148ab3e6f83f0694048a14f5fc83f9
Felipe Alves <felipealves0891@gmail.com>
2025-05-14 21:06:55 -0300

    Ajustado

--- src/Reax.Core/Ast/Statements/ScriptNode.cs
+++ src/Reax.Core/Ast/Statements/ScriptNode.cs
@@ -4,12 +4,21 @@ using Reax.Core.Locations;
 namespace Reax.Core.Ast.Statements;
 
 [ExcludeFromCodeCoverage]
-public record ScriptNode(
-    string Identifier, 
-    ReaxNode[] Nodes, 
-    SourceLocation Location) : StatementNode(Location)
+public record ScriptNode : StatementNode
 {
+    public ScriptNode(
+        string identifier, 
+        ReaxNode[] nodes, 
+        SourceLocation location) : base(location)
+    {
+        Identifier = identifier;
+        Nodes = nodes;
+    }
+
     public override IReaxNode[] Children => Nodes;
+
+    public string Identifier { get; set; }
+    public ReaxNode[] Nodes { get; }
 
     public override void Accept(IReaxInterpreter interpreter)
     {
