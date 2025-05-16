28504156a2cd81d86308ed3e51982f18105e33e9
Felipe Alves <felipealves0891@gmail.com>
2025-05-16 18:31:27 -0300

    Ajustado nome do log

--- src/Reax.Core/Debugger/Logger.cs
+++ src/Reax.Core/Debugger/Logger.cs
@@ -18,49 +18,49 @@ public sealed class Logger : IDisposable
     public static string FormatDate = "yyyy-MM-dd HH:mm:ss.ffffff";
     public static LoggerLevel Level = LoggerLevel.DEBUG;
 
-    public static void LogLexer(string message, [CallerMemberName] string caller = "") 
+    public static void LogLexer(string message, [CallerMemberName] string caller = "")
     {
-        if(!Enabled) return;
+        if (!Enabled) return;
         var formateDate = DateTime.UtcNow.ToString(FormatDate);
         var done = string.Format("DEB [{0}] | Lexer.{2} | {1}", formateDate, message, caller.PadRight(25, ' '));
         _instance.Log(done, LoggerLevel.DEBUG);
     }
-    
-    public static void LogParse(string message, [CallerMemberName] string caller = "") 
+
+    public static void LogParse(string message, [CallerMemberName] string caller = "")
     {
-        if(!Enabled) return;
+        if (!Enabled) return;
         var formateDate = DateTime.UtcNow.ToString(FormatDate);
         var done = string.Format("DEB [{0}] | Parse.{2} | {1}", formateDate, message, caller.PadRight(25, ' '));
         _instance.Log(done, LoggerLevel.DEBUG);
     }
 
-    public static void LogAnalize(string message, [CallerMemberName] string caller = "") 
+    public static void LogAnalize(string message, [CallerMemberName] string caller = "")
     {
-        if(!Enabled) return;
+        if (!Enabled) return;
         var formateDate = DateTime.UtcNow.ToString(FormatDate);
         var done = string.Format("DEB [{0}] | Analizer.{2} | {1}", formateDate, message, caller.PadRight(22, ' '));
         _instance.Log(done, LoggerLevel.DEBUG);
     }
-    
-    public static void LogSemanticContext(string message, [CallerMemberName] string caller = "") 
+
+    public static void LogSemanticContext(string message, [CallerMemberName] string caller = "")
     {
-        if(!Enabled) return;
+        if (!Enabled) return;
         var formateDate = DateTime.UtcNow.ToString(FormatDate);
         var done = string.Format("DEB [{0}] | Context.{2} | {1}", formateDate, message, caller.PadRight(23, ' '));
         _instance.Log(done, LoggerLevel.DEBUG);
     }
-    
-    public static void LogInterpreter(string message, [CallerMemberName] string caller = "") 
+
+    public static void LogInterpreter(string message, [CallerMemberName] string caller = "")
     {
-        if(!Enabled) return;
+        if (!Enabled) return;
         var formateDate = DateTime.UtcNow.ToString(FormatDate);
         var done = string.Format("INF [{0}] | Interpreter.{2} | {1}", formateDate, message, caller.PadRight(20, ' '));
         _instance.Log(done, LoggerLevel.INFO);
     }
-    
-    public static void LogCompile(string message, [CallerMemberName] string caller = "") 
+
+    public static void LogCompile(string message, [CallerMemberName] string caller = "")
     {
-        if(!Enabled) return;
+        if (!Enabled) return;
         var formateDate = DateTime.UtcNow.ToString(FormatDate);
         var done = string.Format("INF [{0}] | Compile.{2} | {1}", formateDate, message, caller.PadRight(23, ' '));
         _instance.Log(done, LoggerLevel.INFO);
@@ -73,18 +73,23 @@ public sealed class Logger : IDisposable
         _instance.Log(done, LoggerLevel.ERROR);
     }
 
-    private readonly StreamWriter _writer = new StreamWriter(@"D:\Source\Scripts\reax-execution.log", true);
-    private void Log(string message, LoggerLevel level) 
+    private readonly StreamWriter _writer = new StreamWriter(Path.Combine(ReaxEnvironment.DirectoryRoot, "logs", $"{DateTime.Now.ToString("yyyy-MM-dd-HHmmss")}.log"), true);
+    private readonly object _locker = new();
+    
+    private void Log(string message, LoggerLevel level)
     {
-        if(((int)level) >= ((int)Level))
+        if (((int)level) >= ((int)Level))
         {
-            _writer.WriteLine(message);
-            _writer.Flush();
-        }            
+            lock (_locker) {
+                _writer.WriteLine(message);
+            }
+
+        }
     }
 
     public void Dispose()
     {
+        _writer.Flush();
         _writer.Dispose();
     }
 }
